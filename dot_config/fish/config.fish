c5fb741f84924cf515a1835a523f22d1ae908989
Daniel <danielwiese@sent.com>
2026-03-15 22:55:56 +0100

    fish + television

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,14 +1,18 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
+set -Ux EDITOR nvim
+
 starship init fish | source
 zoxide init fish | source
-thefuck --alias | source 
+thefuck --alias | source
 
 if status is-interactive
-	tv init fish | source
+    tv init fish | source
 end
 
+abbr -a cd z
+abbr -a vi nvim
+abbr -a vim nvim
 
-alias cd=z
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
