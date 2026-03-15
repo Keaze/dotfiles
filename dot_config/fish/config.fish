003dd13221aa73faa06489afda423802b6a60d7a
Daniel <danielwiese@sent.com>
2026-03-15 15:45:24 +0100

    Fish & Kitty

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,6 +1,12 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 starship init fish | source
 zoxide init fish | source
+
+if status is-interactive
+	tv init fish | source
+end
+
+
 alias cd=z
 # overwrite greeting
 # potentially disabling fastfetch
